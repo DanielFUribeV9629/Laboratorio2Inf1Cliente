60cc83172d09717031d643e2d05c727bcb77aa85
DanielFUribeV9629 <danifelpo@hotmail.com>
2021-11-09 16:15:59 -0500

    Vista inicial Cliente

---
+++ src/labinforma2cliente/LabInforma2Cliente.java
@@ -0,0 +1,14 @@
+package labinforma2cliente;
+
+import vista.Vista;
+
+public class LabInforma2Cliente {
+
+    public static void main(String[] args) {
+        //Paginacion mod= new Paginacion();
+        Vista view = new Vista();       
+        //ControladorParticion control= new ControladorParticion(view, mod);
+        //control.iniciar();
+        view.setVisible(true);   
+    }    
+}
